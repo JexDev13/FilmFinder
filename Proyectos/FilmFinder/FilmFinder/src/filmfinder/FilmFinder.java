ee03637a2130ad316572ce21ddd40df5a5f5367d
JexDev13 <119013519+JexDev13@users.noreply.github.com>
2022-12-27 02:14:01 -0500

    FilmFinderVer0.0

---
+++ Proyectos/FilmFinder/FilmFinder/src/filmfinder/FilmFinder.java
@@ -0,0 +1,11 @@
+package filmfinder;
+
+/*
+ * @autores Cinefilos de EDA
+ */
+public class FilmFinder {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+    }
+}
