d22cb94af8ea4b34345bfb32d4d4bdc6d5ab418a
JexDev13 <119013519+JexDev13@users.noreply.github.com>
2022-12-27 02:57:55 -0500

    FilmFinderVer0.0

--- Proyectos/FilmFinder/FilmFinder/src/Logic/FilmFinder.java
+++ Proyectos/FilmFinder/FilmFinder/src/Logic/FilmFinder.java
@@ -6,6 +6,6 @@ package Logic;
 public class FilmFinder {
 
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
+        System.out.println("Hello World");
     }
 }
